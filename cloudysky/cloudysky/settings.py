678fa84ca46cb8450de10f3d98fd9008c227f4d2
Alicia <asoosai@uchicago.edu>
2025-11-07 13:05:38 -0600

    fixed settings

--- cloudysky/cloudysky/settings.py
+++ cloudysky/cloudysky/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'dev-key-for-class-only'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
