261e665b8544c16148ab1463803aa21155f5d3c4
Alicia <asoosai@uchicago.edu>
2025-11-23 13:23:22 -0600

    edited puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -1,15 +1,6 @@
-import hashlib
-import random
-'''Code to inefficiently and ineffectively scramble a source
-text that requires a little bit of programming to brute force.
-'''
+puzzle_easy_key = 6346
+puzzle_easy_misspell = "Fabruart,"
 
-key = "{:09d}".format(int(random.random() * 1000000000))
+puzzle_key = 485066843
+puzzle_misspell = "wkitpng"
 
-words = open("TEXT", "r").read()
-with open("PUZZLE", "w") as puzzle:
-    for word in words.split():
-        puzzle.write(
-            hashlib.md5(key.encode("utf-8") +
-            word.encode("utf-8")).hexdigest() + "\n"
-            )
